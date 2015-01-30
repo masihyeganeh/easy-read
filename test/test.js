454f5621c49e7e9c615ed8559ca6ed79867bf774
HelloCodeMing <huanmingwong@163.com>
2015-01-30 19:38:37 +0800

    limit jsdom version to 2.x

--- test/test.js
+++ test/test.js
@@ -1,6 +1,11 @@
 var read = require('../index');
 
 read('https://www.byvoid.com/zhs/blog/trans-siberia-travel-notes', function(res) {
-        console.log(res);
+        console.log(res.title);
+        console.log(res.content);
 });
 
+read('http://wanghuanming.com/2015/01/summary-for-2014/', function(res) {
+        console.log(res.title);
+        console.log(res.content);
+});
